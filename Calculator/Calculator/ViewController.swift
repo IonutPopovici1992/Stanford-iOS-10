c1c562b4e1f838ac12e65feb6458cb6c50a8c571
John <john@Johns-MacBook-Pro.local>
2017-08-16 14:54:20 +0300

    Changes for Main.storyboard and ViewController.swift

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,10 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
     @IBAction func touchDigit(_ sender: UIButton) {
         // drawHorizontalLine(from: 5.0, to: 8.5, using: UIColor.blue)
         let digit = sender.currentTitle!
-        print("\(digit) was touched!")
+        // print("\(digit) was touched!")
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+                case "π":
+                    displayValue = Double.pi
+                case "√":
+                    displayValue = sqrt(displayValue)
+                default:
+                    break
+            }
+        }
     }
     
 //    func drawHorizontalLine(from startX: Double, to endX: Double, using color: UIColor) {
